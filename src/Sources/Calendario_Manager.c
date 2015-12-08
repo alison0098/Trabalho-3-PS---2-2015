844b8114b41b026948bfb78fde737e16513fe9b4
Guilherme Okita <gui_okita@hotmail.com>
2015-12-09 01:53:27 +0900

    Busca de palestras
    Adicionado a busca de palestras por dia, mes ou ano

--- src/Sources/Calendario_Manager.c
+++ src/Sources/Calendario_Manager.c
@@ -11,7 +11,7 @@
 
 static FILE* arquivo_bom = NULL;
 static int controle_vetor = 0;
-static Calendario* vetor_acertado[20];
+static Calendario** vetor_acertado;
 
 typedef struct Tree{
 	Calendario* nodo;
@@ -29,7 +29,7 @@ Tree* Tree_New(Calendario* nodo){
 
 void Tree_Add(Tree* arv, Calendario* nodo){
 	if(arv != NULL){
-		printf("%d",Disp_Comp(Calendario_Get_Disponibilidade(arv->nodo), Calendario_Get_Disponibilidade(nodo)));
+		//printf("%d",Disp_Comp(Calendario_Get_Disponibilidade(arv->nodo), Calendario_Get_Disponibilidade(nodo)));
 		if(Disp_Comp(Calendario_Get_Disponibilidade(arv->nodo), Calendario_Get_Disponibilidade(nodo)) > 0){
 			if(arv->esq != NULL)
 				Tree_Add(arv->esq, nodo);
@@ -90,9 +90,8 @@ void Calendario_Manager_Add(Calendario* cal){
 
 void Calendario_Manager_Print(){
 	Tree* arvore;
-	int size = 0, i, dia, check = 0, mes, checkm = 0, ano;
+	int i, dia, check = 0, mes, checkm = 0, ano = 0;
 	Disponibilidade* disp;
-	char data[15];
 	Palestra* p;
 	Local* l;
 	Palestrante* p2;
@@ -102,6 +101,8 @@ void Calendario_Manager_Print(){
 	for(i = 1; i < Calendario_Vector_Size; i++){
 		Tree_Add(arvore, calendario[i]);
 	}
+
+	vetor_acertado = (Calendario**)malloc(Calendario_Vector_Size*sizeof(Calendario*));
 
 	Tree_Fix_Vector(arvore);
 
@@ -121,7 +122,7 @@ void Calendario_Manager_Print(){
 	    	p2 = Calendario_Get_Palestrante(vetor_acertado[i]);
 	    	disp = Calendario_Get_Disponibilidade(vetor_acertado[i]);
 	    	if(mes != checkm){
-	    		fprintf(arquivo_bom, "Mes %d//%d\n", mes);
+	    		fprintf(arquivo_bom, "Mes %d//\n", mes);
 	    		checkm = mes;
 	    	}
 	    	if(dia != check){
@@ -133,8 +134,70 @@ void Calendario_Manager_Print(){
 					Disponibilidade_Get_Hora(disp) + Palestra_Get_Dur(p)/60, Disponibilidade_Get_Minuto(disp) + Palestra_Get_Dur(p)%60,
 					Local_Get_Nome(l), Local_Get_Endereco(l));
 	}
-	Tree_Del(arvore);
-	//free(arvore);
 	fclose(arquivo_bom);
-        free(calendario);
+
+	Tree_Del(arvore);
+	free(arvore);
+}
+
+void Calendario_Manager_Delete(){
+	fclose(arquivo_bom);
+	free(calendario);
+}
+
+void Calendario_Manager_Search_By_Day(int dia){
+    int i = 0;
+    int check = 0;
+
+	for(i = 0; i < Calendario_Vector_Size; i++){
+        if(dia == Disponibilidade_Get_Dia(Calendario_Get_Disponibilidade(vetor_acertado[i]))){
+        	check = 1;
+        	printf("Palestra: %s\n", Palestra_Get_Nome(Calendario_Get_Palestra(vetor_acertado[i])));
+        	printf("Palestrante: %s\n", Palestra_Get_Nome_Palestrante(Calendario_Get_Palestra(vetor_acertado[i])));
+        	printf("Local: %s - %02d:%02d\n", Palestra_Get_Local(Calendario_Get_Palestra(vetor_acertado[i])), Disponibilidade_Get_Hora(Calendario_Get_Disponibilidade(vetor_acertado[i])),
+        			Disponibilidade_Get_Minuto(Calendario_Get_Disponibilidade(vetor_acertado[i])));
+        }
+	}
+	if(check == 0)
+		printf("Nao foram encontrados nenhuma palestra para esse dia!\n");
+
+	return;
+}
+
+void Calendario_Manager_Search_By_Month(int mes){
+	int i = 0;
+	int check = 0;
+
+	for(i = 0; i < Calendario_Vector_Size; i++){
+	    if(mes == Disponibilidade_Get_Mes(Calendario_Get_Disponibilidade(vetor_acertado[i]))){
+	       	check = 1;
+	       	printf("Palestra: %s\n", Palestra_Get_Nome(Calendario_Get_Palestra(vetor_acertado[i])));
+	       	printf("Palestrante: %s\n", Palestra_Get_Nome_Palestrante(Calendario_Get_Palestra(vetor_acertado[i])));
+	       	printf("Local: %s - %02d:%02d\n", Palestra_Get_Local(Calendario_Get_Palestra(vetor_acertado[i])), Disponibilidade_Get_Hora(Calendario_Get_Disponibilidade(vetor_acertado[i])),
+	       			Disponibilidade_Get_Minuto(Calendario_Get_Disponibilidade(vetor_acertado[i])));
+	       }
+	}
+	if(check == 0)
+		printf("Nao foram encontrados nenhuma palestra para esse dia!\n");
+
+	return;
+}
+
+void Calendario_Manager_Search_By_Year(int ano){
+	int i = 0;
+	int check = 0;
+
+	for(i = 0; i < Calendario_Vector_Size; i++){
+	    if(ano == Disponibilidade_Get_Ano(Calendario_Get_Disponibilidade(vetor_acertado[i]))){
+	       	check = 1;
+	       	printf("Palestra: %s\n", Palestra_Get_Nome(Calendario_Get_Palestra(vetor_acertado[i])));
+	       	printf("Palestrante: %s\n", Palestra_Get_Nome_Palestrante(Calendario_Get_Palestra(vetor_acertado[i])));
+	       	printf("Local: %s - %02d:%02d\n", Palestra_Get_Local(Calendario_Get_Palestra(vetor_acertado[i])), Disponibilidade_Get_Hora(Calendario_Get_Disponibilidade(vetor_acertado[i])),
+	       			Disponibilidade_Get_Minuto(Calendario_Get_Disponibilidade(vetor_acertado[i])));
+	    }
+	}
+	if(check == 0)
+		printf("Nao foram encontrados nenhuma palestra para esse dia!\n");
+
+	return;
 }
--- src/Sources/main.c
+++ src/Sources/main.c
@@ -14,6 +14,8 @@
 #include <stdio.h>
 
 int main(){
+	int escolha = 0;
+	int valor = 0;
     PalestraManager_Read("palestras.txt");
     PalestraManager_Print();
 
@@ -28,6 +30,29 @@ int main(){
     Organizer_Organize_Filas(LocalManager_GetLocal(), PalestraManager_Get(), PalestranteManager_GetPalestrante());
     Calendario_Manager_Print();
 
+    while(escolha != -1){
+    	printf("Menu:\n1 - Procurar por dia\n2 - Procurar por mes\n3 - Procurar por ano\n-1 - Sair\nEscolha: ");
+    	scanf("%d", &escolha);
+    	switch(escolha){
+    	case 1:
+    		printf("Dia a ser procurado: ");
+    		scanf("%d", &valor);
+    		Calendario_Manager_Search_By_Day(valor);
+    		break;
+    	case 2:
+    		printf("Mes a ser procurado: ");
+    		scanf("%d", &valor);
+    		Calendario_Manager_Search_By_Month(valor);
+    		break;
+    	case 3:
+    		printf("Ano a ser procurado: ");
+    		scanf("%d", &valor);
+    		Calendario_Manager_Search_By_Year(valor);
+    		break;
+    	}
+    }
+
+    Calendario_Manager_Delete();
     PalestraManager_PopFila();
     PalestranteManager_PopFila();
     LocalManager_PopFila();
